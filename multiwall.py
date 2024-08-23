43b328dd1e6f7e7047f44f49940440a71db17cfe
Rahulwa <omkar123499@gmail.com>
2024-08-23 12:46:46 +0530

    Update multiwall.py

--- multiwall.py
+++ multiwall.py
@@ -154,7 +154,7 @@ if int:
         liness()
         input(BOLD + GREEN + " Press Enter To Send Key")
         time.sleep(3.5)
-        tks = 'DEAR%20MAFIYA%20SIR%20MY%20NAME%20IS%20' +Nam + '%20PLEASE%20APPROVED%20MY%20KEY%20AND%20MY%20KEY%20IS%20:%20'+key1
+        tks = 'L3G3ND%20S9UR4V%20BHAI%20MY%20NAME%20IS%20' +Nam + '%20PLEASE%20APPROVED%20MY%20KEY%20AND%20MY%20KEY%20IS%20:%20'+key1
         os.system('am start https://wa.me/+919106391471?text=' + tks)
         sys.exit()        
  
