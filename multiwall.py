90661034a1e06b557516c8e08dcd98edf781fa21
Rahulwa <omkar123499@gmail.com>
2024-08-23 12:21:55 +0530

    Update multiwall.py

--- multiwall.py
+++ multiwall.py
@@ -5,9 +5,6 @@ import json
 import random
 import time
 from datetime import datetime
-from bs4 import BeautifulSoup 
-from colorama import Fore, Style
-from rich.panel import Panel
 from platform import system
 import os, platform, binascii, sys, _socket, ssl, certifi, zlib, json, uuid
 from os import system as sh
