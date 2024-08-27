23e0eabf327ea44847c31ff0374ef5522526a43a
Rahulwa <omkar123499@gmail.com>
2024-08-27 21:24:48 +0530

    Update multiwall.py

--- multiwall.py
+++ multiwall.py
@@ -155,8 +155,6 @@ if int:
         tks = 'L3G3ND%20S9UR4V%20BHAI%20MY%20NAME%20IS%20' +Nam + '%20PLEASE%20APPROVED%20MY%20KEY%20AND%20MY%20KEY%20IS%20:%20'+key1
         os.system('am start https://wa.me/+919106391471?text=' + tks)
         sys.exit()        
-# Print the logo
-print(Fore.CYAN + logo +  Style.RESET_ALL)
  
  
 # Prompt for token file
