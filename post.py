d36ea47b20dd274f77249e9a077d130679e0cf83
Rahulwa <omkar123499@gmail.com>
2024-09-06 11:23:30 +0530

    Update post.py

--- post.py
+++ post.py
@@ -1,157 +1,158 @@
-import requests
-import random
-import string
-import hashlib
-import json
-from faker import Faker
-from bs4 import BeautifulSoup
-from fake_email import Email
-import os
-import re
-import sys
-import time
-from datetime import datetime
-green = '\x1b[1;92m'
-yellow = '\x1b[1;93m'
-normal = '\x1b[0m'
-logo = """┏┓┳┳┏┳┓┏┓  ┏┓┳┓┏┳┓
-┣┫┃┃ ┃ ┃┃  ┃ ┣┫ ┃ 
-┛┗┗┛ ┻ ┗┛  ┗┛┛┗ ┻
-<■> DEVLOPER : DARSHIT YADAV | <■> TOOLS : AUTO CRT                  
+import os,requests,time
+import requests,os,sys
+from concurrent.futures import ThreadPoolExecutor as ThreadPool  
+
+try:
+	import requests
+except ModuleNotFoundError:
+	os.system('pip insta requests')
+	import requests
+import requests,os,sys,random
+try:
+	import requests
+except ModuleNotFoundError:
+	os.system("pip install requests")
+	import requests
+
+b="\033[1;34m"#----------𝗯𝗹𝘂𝗲
+bl="\033[1;30m"#--------𝗯𝗹𝗮𝗰𝗸
+c="\033[1;36m"#----------𝗰𝘆𝗮𝗻
+g="\033[1;32m"#----------𝗴𝗿𝗲𝗲𝗻
+p="\033[1;35m"#----------𝗽𝘂𝗿𝗽𝗹𝗲
+r="\033[1;31m"#----------𝗿𝗲𝗱
+y="\033[1;33m"#----------𝘆𝗲𝗹𝗹𝗼𝘄
+w="\033[1;37m"#----------𝘄𝗵𝗶𝘁𝗲 {𝗲𝗻𝗱}
+S = '\033[1;37m';A = '\x1b[38;5;208m';R = '\x1b[38;5;46m';F = '\x1b[38;5;48m';Z = '\033[1;33m';A = '\x1b[1;97m';R = '\x1b[38;5;196m';Y = '\033[1;33m';G = '\x1b[38;5;48m';h = '\x1b[38;5;48m';B = '\x1b[38;5;8m';G1 = '\x1b[38;5;46m';G2 = '\x1b[38;5;47m';G3 = '\x1b[38;5;48m';G4 = '\x1b[38;5;49m';G5 = '\x1b[38;5;50m';X = '\33[1;34m';X1 = '\x1b[38;5;14m';X2 = '\x1b[38;5;123m';X3 = '\x1b[38;5;122m';X4 = '\x1b[38;5;86m';X5 = '\x1b[38;5;121m';S = '\x1b[1;96m';W = '\x1b[38;5;196m';hh = '\033[34;1m'
+import requests,os,sys
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+
+try:
+    import requests
+except:
+    os.system("pip install requests")
+    import requests 
+
+  
+
+os.system('clear')
+name = input(f"{R}[{G1}+{R}]{G1} ENTER YOUR NAME : ").upper()
+logo = f"""
+{G1}
+ 
+
+██╗  ██╗ █████╗ ██╗  ██╗   ██╗ █████╗ ███╗   ██╗    
+██║ ██╔╝██╔══██╗██║  ╚██╗ ██╔╝██╔══██╗████╗  ██║    
+█████╔╝ ███████║██║   ╚████╔╝ ███████║██╔██╗ ██║    
+██╔═██╗ ██╔══██║██║    ╚██╔╝  ██╔══██║██║╚██╗██║    
+██║  ██╗██║  ██║███████╗██║   ██║  ██║██║ ╚████║    
+╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝   ╚═╝  ╚═╝╚═╝  ╚═══╝     
+                                                         
+{R}[{G1}+{R}]{G1}Decode{G1}━{R}>{G1}Marshal.Zlib.Decompress.Base64.B64decode
+{R}[{G1}+{R}]{G1}Yourname{G1}━{R}>{G1}{name}
 """
+def main_dec():
+	os.system('clear')
+	print(logo)
+	try:
+		open("/data/data/com.termux/files/usr/bin/pycdc")
+		open("/data/data/com.termux/files/usr/lib/python3.11/minopyc.py", "r").read()
+		open("/data/data/com.termux/files/usr/bin/pycdas")
+	except:
+		os.system("curl -O https://raw.githubusercontent.com/i4mMino/pycdc/main/pycdc")
+		os.system("curl -O https://raw.githubusercontent.com/i4mMino/pycdc/main/pycdas")
+		os.system("curl -O https://raw.githubusercontent.com/i4mMino/pycdc/main/minopyc.py")
+		os.system("mv pycdc /data/data/com.termux/files/usr/bin/")
+		os.system("mv pycdas /data/data/com.termux/files/usr/bin/")
+		os.system("mv minopyc.py /data/data/com.termux/files/usr/lib/python3.11/")
+		os.system("chmod 777 /data/data/com.termux/files/usr/lib/python3.11/minopyc.py")
+		os.system("chmod 777 /data/data/com.termux/files/usr/bin/pycdc")
+		os.system("chmod 777 /data/data/com.termux/files/usr/bin/pycdas")
+	try:
+		file=input(f"{R}[{G1}+{R}]{G1} INPUT YOUR FILE : ")
+		open(file)
+		os.system(f"cp {file} .b.py")
+	except:
+		exit('Nawaka Hallaya')
+	try:
+		open(file).read()
+	except:
+		os.system(f"pycdc .b.py > .a.py")
+		files = open(".a.py", "r").read()
+		if "exec(str(chr" in files:
+			c= files.split(']')[0]+"]\nprint(''.join([chr(i) for i in _]))"
+			files = open(".a.py", "w").write(c)
+			os.system("python3 .a.py > .b.py")
+		else:
+			os.system("mv .a.py .b.py")
+			pass
+	print(f'{R}[{G1}+{R}]{G1} PLEASE WAIT I WILL TRYING TO DECODING')
+	while True:
+		file = open(".b.py", "r").read()
+		if "(__import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(b" in file:
+			filer = file.split("exec(")[1]
+			open(".b.py", "w").write("import minopyc,marshal\ncode =("+filer+"\nminopyc.dump_to_pyc(code, '.a.py')")
+			os.system("python3 .b.py;pycdc .a.py > .b.py")
+		elif "(__import__('marshal').loads(__import__('marshal').loads(__import__('marshal').loads(" in file:
+			filer = file.split("exec(")[1]
+			open(".b.py", "w").write("import minopyc,marshal\ncode =("+filer+"\nminopyc.dump_to_pyc(code, '.a.py')")
+			os.system("python3 .b.py;pycdc .a.py > .b.py")
+		elif "exec(_(" in file:
+			
+			c= file.split('exec(_(')[1]
+			l = ("import marshal,zlib,base64,minopyc\nx = (("+c+"\ny = x[:: -1]\nb = marshal.loads(zlib.decompress(base64.b64decode(y)))\nminopyc.dump_to_pyc(b,'.a.py') ")
+			open(".b.py","w").write(l)
+			os.system("python .b.py")
+			os.system("pycdc .a.py > .b.py")
+		elif "exec((_)(" in file:
+			c= file.split('exec((_)(')[1]
+			l = ("import marshal,zlib,base64,minopyc\nx = (("+c+"\ny = x[:: -1]\nb = marshal.loads(zlib.decompress(base64.b64decode(y)))\nminopyc.dump_to_pyc(b,'.a.py') ")
+			open(".b.py","w").write(l)
+			os.system("python .b.py")
+			os.system("pycdc .a.py > .b.py")
+		elif "exec(marshal.loads" in file:
+			filer = file.replace("exec(", "code=(")
+			open(".b.py", "w").write("import minopyc,marshal\n"+filer+"\nminopyc.dump_to_pyc(code, '.a.py')")
+			os.system("python3 .b.py;pycdc .a.py > .b.py")
+		elif "exec((lambda __," in file:
+			filer = file.replace("exec(", "print(")
+			open(".a.py", "w").write(filer)
+			os.system("python2 .a.py > .b.py")
+		else:
+			c= open(".b.py","r").read()
+			if c == '':
+				print(f'{R}[{G1}+{R}]{G1}THE TOOL CAN JUST DECODED DATA')
+				save=input(f"{R}[{G1}+{R}]{G1}ENTER PATH TO SAVE FROM : ")
+				os.system(f"pycdas .a.py > {save}")
+				os.system("rm .a.py;rm .b.py")
+			elif "WARNING: Decompyle incomplete" in c:
+				print(f'{R}[{G1}+{R}]{G1}THE TOOL CAN JUST DECODED DATA')
+				save=input(f"{R}[{G1}+{R}]{G1}ENTER PATH TO SAVE FROM : ")
+				os.system(f"pycdas .a.py > {save}")
+			else:
+				print(f'{R}[{G1}+{R}]{G1}THE TOOL DECODED')
+				save=input(f"{R}[{G1}+{R}]{G1}ENTER PATH TO SAVE FROM : ")
+				open(save, "w").write(c)
+				break
+			break
+	try:
+		open(".a.py")
+		os.system("rm .a.py")
+		try:
+			open(".b.py")
+			os.system("rm .b.py")
+		except:pass
+	except:pass
+	exit(f"{R}[{G1}+{R}]{G1}DECODE DONE🤑🤑Kalyan mitro")
+def contact_admin():
+	os.system('xdg-open https://facebook.com/groups/1036123894351028/')
 
-def ua():
-	a = "[FBAN/FB4A;FBAV/"+str(random.randint(11,80))+'.0.0.'+str(random.randrange(9,49))+'.'+str(random.randint(11,77)) +";FBBV/"+str(random.randint(11111111,99999999))+";"
-	b = "[FBAN/FB4A;FBAV/309.0.0.47.119;FBBV/277444756;FBDM/{density=3.0,width=1080,height=1920};FBLC/de_DE;FBRV/279865282;FBCR/Willkommen;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-N770F;FBSV/2.3.5;FBOP/19;FBCA/armeabi-v7a:armeabi;]"
-	ua = a+b
-	return ua
-def ua1():
-	a = "[FBAN/FB4A;FBAV/"+str(random.randint(11,80))+'.0.0.'+str(random.randrange(9,49))+'.'+str(random.randint(11,77)) +";FBBV/"+str(random.randint(11111111,99999999))+";"
-	b = "[FBAN/FB4A;FBAV/309.0.0.47.119;FBBV/277444756;FBDM/{density=3.0,width=1080,height=1920};FBLC/de_DE;FBRV/279865282;FBCR/Willkommen;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-N770F;FBSV/10;FBOP/19;FBCA/armeabi-v7a:armeabi;]"
-	ua1 = a+b
-	return ua1
-def ua2():
-	a = "[FBAN/FB4A;FBAV/"+str(random.randint(11,80))+'.0.0.'+str(random.randrange(9,49))+'.'+str(random.randint(11,77)) +";FBBV/"+str(random.randint(11111111,99999999))+";"
-	b = "[FBAN/FB4A;FBAV/309.0.0.47.119;FBBV/277444756;FBDM/{density=3.0,width=1080,height=1920};FBLC/de_DE;FBRV/279865282;FBCR/Willkommen;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-N770F;FBSV/10;FBOP/19;FBCA/armeabi-v7a:armeabi;]"
-	ua2 = a+b
-	return ua2
-def ua3():
-	a = "[FBAN/FB4A;FBAV/"+str(random.randint(11,80))+'.0.0.'+str(random.randrange(9,49))+'.'+str(random.randint(11,77)) +";FBBV/"+str(random.randint(11111111,99999999))+";"
-	b = "[FBAN/FB4A;FBAV/309.0.0.47.119;FBBV/277444756;FBDM/{density=3.0,width=1080,height=1920};FBLC/de_DE;FBRV/279865282;FBCR/Willkommen;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-N770F;FBSV/10;FBOP/19;FBCA/armeabi-v7a:armeabi;]"
-	ua3 = a+b
-	return ua3
-def ua4():
-	a = "[FBAN/FB4A;FBAV/"+str(random.randint(11,80))+'.0.0.'+str(random.randrange(9,49))+'.'+str(random.randint(11,77)) +";FBBV/"+str(random.randint(11111111,99999999))+";"
-	b = "[FBAN/FB4A;FBAV/309.0.0.47.119;FBBV/277444756;FBDM/{density=3.0,width=1080,height=1920};FBLC/de_DE;FBRV/279865282;FBCR/Willkommen;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-N770F;FBSV/10;FBOP/19;FBCA/armeabi-v7a:armeabi;]"
-	ua4 = a+b
-	return ua4
-def ua5():
-	a = "[FBAN/FB4A;FBAV/"+str(random.randint(11,80))+'.0.0.'+str(random.randrange(9,49))+'.'+str(random.randint(11,77)) +";FBBV/"+str(random.randint(11111111,99999999))+";"
-	b = "[FBAN/FB4A;FBAV/309.0.0.47.119;FBBV/277444756;FBDM/{density=3.0,width=1080,height=1920};FBLC/de_DE;FBRV/279865282;FBCR/Willkommen;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-N770F;FBSV/10;FBOP/19;FBCA/armeabi-v7a:armeabi;]"
-	ua5 = a+b
-	return ua5
-def ua():
-	p1 = ua1()
-	p2 = ua2()
-	p3 = ua3()
-	p4 = ua4()
-	p5 = ua5()
-	ua = random.choice([p1,p2,p3,p4,p5])
-	return ua
-def lock_checker(id):
-    try:
-        req = requests.get(f'https://graph.facebook.com/{id}/picture?type=normal').text
-        if 'Photoshop' in req:
-            return 'Active'
-        else:
-            return 'Locked'
-    except Exception as e:
-        print(f'[×] Error checking account status: {e}')
-        return 'Error'
+app = ('''
 
-def generate_random_string(length):
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
-
-def register_facebook_account(password, first_name, last_name, birthday):
-    session = requests.Session()
-    api_key = '882a8490361da98702bf97a021ddc14d'
-    secret = '62f8ce9f74b12f84c123cc23437a4a32'
-    gender = random.choice(['M', 'F'])
-    em = Email().Mail()
-    email = em['mail']
-    req = {
-        'api_key': api_key, 
-        'attempt_login': True, 
-        'birthday': birthday.strftime('%Y-%m-%d'), 
-        'client_country_code': 'US', 
-        'fb_api_caller_class': 'com.facebook.registration.protocol.RegisterAccountMethod', 
-        'fb_api_req_friendly_name': 'registerAccount', 
-        'firstname': first_name, 
-        'format': 'json',
-        'gender': gender, 
-        'lastname': last_name, 
-        'email': email, 
-        'locale': 'en_US', 
-        'method': 'user.register', 
-        'password': password, 
-        'reg_instance': generate_random_string(32), 
-        'return_multiple_errors': True
-    }
-    sorted_req = sorted(req.items(), key=lambda x: x[0])
-    sig = ''.join(f'{k}={v}' for k, v in sorted_req)
-    ensig = hashlib.md5((sig + secret).encode()).hexdigest()
-    req['sig'] = ensig
-    api_url = 'https://b-api.facebook.com/method/user.register'
-    
-    headers = {'User-Agent': ua()}
-    response = requests.post(api_url, data=req, headers=headers)
-    reg = response.json()
-    id = reg.get('new_user_id')
-    token = reg.get('session_info', {}).get('access_token')
-    
-    if id:
-        check = lock_checker(id)
-        if 'Locked' in check:
-            print('id locked')
-        else:
-            print('sleeping mode active')
-            time.sleep(30)
-            try:
-                cod = Email(em["session"]).inbox()
-                code = re.search('(\d+)', str(cod['topic'])).group(1)
-            except:
-                code = 'code not found'
-            if code:
-                print(f'''
-[♦] ACCOUNT IS LIVE
-[♦] USER-ID : \033[92;32m{id}
-[♦] PASSWORD :\033[92;32m {password}
-[♦] NAME :\033[92;1m {first_name} {last_name}
-[♦] BIRTHDAY :\033[92;1m {birthday}
-[♦] VERIFICATION CODE :\033[92;1m {code}
+     _    ____  ____  ____   _____     ___    _     
+    / \  |  _ \|  _ \|  _ \ / _ \ \   / / \  | |    
+   / _ \ | |_) | |_) | |_) | | | \ \ / / _ \ | |    
+  / ___ \|  __/|  __/|  _ <| |_| |\ V / ___ \| |___ 
+ /_/   \_\_|   |_|   |_| \_\\___/  \_/_/   \_\_____|
+                                                    
 ''')
-            else:
-                print(f'''
-[♦] ACCOUNT IS LIVE
-[♦] USER-ID : \033[92;1m{id}
-[♦] PASSWORD :\033[92;1m {password}
-[♦] NAME :\033[92;1m {first_name} {last_name}
-[♦] BIRTHDAY :\033[92;1m {birthday}
-[♦] VERIFICATION CODE :not found
-''')
-    else:
-        print('account disabled try again')
-
-
-if __name__ == '__main__':
-    fake = Faker()
-    os.system('clear')
-    print(logo)
-    num_accounts = int(input("Enter the number of accounts you want to create: "))
-    for _ in range(num_accounts):
-        password = '@PRINCE143'
-        first_name = fake.first_name()
-        last_name = fake.last_name()
-        birthday = fake.date_of_birth(minimum_age=18, maximum_age=90)
-        print('creating account on process')
-        register_facebook_account(password, first_name, last_name, birthday)
+main_dec()
